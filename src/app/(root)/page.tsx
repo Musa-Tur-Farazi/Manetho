9e401768e9c98d00276f5f5fece94363ef735c49
Musa Tur Farazi <musatur330@gmail.com>
2025-04-17 16:07:48 +0600

    initial commit

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -10,12 +10,11 @@ import HeroSection from "../../../components/landingpage/section/HeroSection";
 function Landingpage() {
   const { isSignedIn } = useAuth();
   return (
-    
+
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-indigo-50">
       <div className="absolute inset-0 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:40px_40px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
 
       <Navbar />
-
       <main className="relative">
         <HeroSection />
         <FeatureCarousel />
